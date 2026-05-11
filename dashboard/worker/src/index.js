b9b4ea09441fdab9f558cbaa920f000014c765cd
Javier Alejandro Garcia <garcia.javier.alejandro@gmail.com>
2026-05-11 19:11:45 -0300

    Enhance CORS support by adding allowed methods and headers in jsonResponse

--- dashboard/worker/src/index.js
+++ dashboard/worker/src/index.js
@@ -1161,6 +1161,8 @@ function jsonResponse(data, { status = 200, origin = "*" } = {}) {
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
       "Access-Control-Allow-Origin": origin || "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Cache-Control": "no-store",
     },
   });
